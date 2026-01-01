c6bd5379d43fefba84ea60dfae25b7822a21e083
Alex Dombroski <alexdombroski1@gmail.com>
2026-01-01 11:16:21 -0700

    chore: add note about stash -u

--- src/commands/files.ts
+++ src/commands/files.ts
@@ -1,6 +1,12 @@
 import { getArgList } from '../utils/args';
 import { spawnCommand } from '../utils/commands';
 
+/**
+TODO - This doesn't fully work with git stash -u
+Need to run this command to see untracked files
+git ls-tree -r --name-only stash@{0}^3 
+*/
+
 export function files() {
 	spawnCommand('git', ['show', '--name-only', '--pretty=format:', ...getArgList()], {
 		stdio: 'inherit',
