4c542d19c038f8f9dd78b175ddc0551cf3fa8fd2
Alex Dombroski <alexdombroski1@gmail.com>
2026-01-23 16:06:48 -0700

    fix: variable modal width is much larger

--- src/components/hooks/useDimensions.ts
+++ src/components/hooks/useDimensions.ts
@@ -4,14 +4,8 @@ export function useDimensions() {
   const { stdout } = useStdout();
   const sectionHeight = stdout.rows - 2;
   const sectionHalfHeight = Math.floor(sectionHeight / 2);
-  let modalWidth;
-  if (stdout.columns > 64) {
-    modalWidth = 60;
-  } else if (stdout.columns > 60) {
-    modalWidth = 48;
-  } else {
-    modalWidth = stdout.columns - 4;
-  }
+  const modalWidth = stdout.columns > 100 ? 96 : stdout.columns - 4;
+
   return {
     width: stdout.columns,
     height: stdout.rows,
