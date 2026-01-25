df2579dab67f5cf1d4b111a56279edf75d1ef6d9
Alex Dombroski <alexdombroski1@gmail.com>
2026-01-24 23:12:18 -0700

    feat: readd temporary status page

--- src/pages/status/index.tsx
+++ src/pages/status/index.tsx
@@ -1,12 +1,21 @@
 import { Box, Text } from 'ink';
 import { useDimensions } from '../../components/hooks/useDimensions';
+import { Section } from '../../components/section';
+import { useResolved } from '../../components/hooks/useResolved';
+import { getStatusPorcelain } from '../../utils/git';
 
 function AllSections() {
   const { width, sectionHeight } = useDimensions();
 
+  const { value: status } = useResolved(getStatusPorcelain);
+
   return (
     <Box width={width} height={sectionHeight}>
-      <Text>TODO - Status Page</Text>
+      <Section title="Status" innerHeight={sectionHeight - 1}>
+        {status?.map((file) => (
+          <Text key={file}>{file}</Text>
+        ))}
+      </Section>
     </Box>
   );
 }
