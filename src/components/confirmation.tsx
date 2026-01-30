c6c5b267db0cb2f16769b7357f30dbc80d356de9
Alex Dombroski <alexdombroski1@gmail.com>
2026-01-30 08:53:55 -0700

    fix: allow wipe to run on the enter key

--- src/components/confirmation.tsx
+++ src/components/confirmation.tsx
@@ -3,33 +3,34 @@ import { useState } from 'react';
 import { WithProgress } from './withProgress';
 
 type Props = {
-	prompt: string;
-	msg: string;
-	onConfirm: () => Promise<string>;
+  prompt: string;
+  msg: string;
+  onConfirm: () => Promise<string>;
+  requireConfirmation?: boolean;
 };
 
-export function Confirmation({ prompt, msg, onConfirm }: Props) {
-	const [answer, setAnswer] = useState<string>('');
-	const { exit } = useApp();
+export function Confirmation({ prompt, msg, onConfirm, requireConfirmation }: Props) {
+  const [answer, setAnswer] = useState<string>('');
+  const { exit } = useApp();
 
-	useInput((input, key) => {
-		if (answer) return;
+  useInput((input, key) => {
+    if (answer) return;
 
-		const normalized = input.toLowerCase();
-		if (normalized === 'y') {
-			setAnswer('Yes');
-		} else if (normalized === 'n' || key.return) {
-			setAnswer('No');
-			exit();
-		}
-	});
+    const normalized = input.toLowerCase();
+    if (normalized === 'y' || (key.return && !requireConfirmation)) {
+      setAnswer('Yes');
+    } else if (normalized === 'n' || (key.return && requireConfirmation)) {
+      setAnswer('No');
+      exit();
+    }
+  });
 
-	return (
-		<>
-			<Text>
-				{prompt} [y/N] {answer}
-			</Text>
-			{answer === 'Yes' && <WithProgress msg={msg} promise={onConfirm()} />}
-		</>
-	);
+  return (
+    <>
+      <Text>
+        {prompt} [{requireConfirmation ? 'y/N' : 'Y/n'}] {answer}
+      </Text>
+      {answer === 'Yes' && <WithProgress msg={msg} promise={onConfirm()} />}
+    </>
+  );
 }
